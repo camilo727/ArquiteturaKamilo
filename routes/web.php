ee0f8d0cca506abf9035c95ad0f37f24d10dcd03
camilo727 <53825460+camilo727@users.noreply.github.com>
2021-10-26 17:16:16 -0500

    rutas

--- routes/web.php
+++ routes/web.php
@@ -13,28 +13,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('/','App\Http\Controllers\homeController');
-Route::resource('niña','App\Http\Controllers\chicaController');
-Route::resource('mujer','App\Http\Controllers\mujerController');
-Route::resource('pedidos','App\Http\Controllers\pedidoController');
-Route::resource('envio','App\Http\Controllers\envioController');
-Route::resource('compra','App\Http\Controllers\compraController');
-// Route::get('/', function () {
-//      return view('cliente/home/home');
-//  });
+// Route::resource('/','App\Http\Controllers\homeController');
+// Route::resource('niña','App\Http\Controllers\chicaController');
+// Route::resource('mujer','App\Http\Controllers\mujerController');
+// Route::resource('pedidos','App\Http\Controllers\pedidoController');
+// Route::resource('envio','App\Http\Controllers\envioController');
+// Route::resource('compra','App\Http\Controllers\compraController');
+Route::get('/', function () {
+     return view('cliente/home/home');
+ });
  
-//  Route::get('/niña', function () {
-//      return view('cliente/home/niña');
-//  });
-//  Route::get('/mujer', function () {
-//      return view('cliente/home/mujer');
-//  });
-//  Route::get('/pedidos', function () {
-//      return view('cliente/home/pedidos');
-//  });
-//  Route::get('/envio', function () {
-//      return view('cliente/home/envio');
-//  });
-//  Route::get('/compra', function () {
-//      return view('cliente/home/compra');
-//  });
+ Route::get('/niña', function () {
+     return view('cliente/home/niña');
+ });
+ Route::get('/mujer', function () {
+     return view('cliente/home/mujer');
+ });
+ Route::get('/pedidos', function () {
+     return view('cliente/home/pedidos');
+ });
+ Route::get('/envio', function () {
+     return view('cliente/home/envio');
+ });
+ Route::get('/compra', function () {
+     return view('cliente/home/compra');
+ });
