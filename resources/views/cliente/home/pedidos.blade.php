c6e7019e3a1c6a1e422b529dfb7c637e228d7880
camilo727 <53825460+camilo727@users.noreply.github.com>
2021-10-26 08:49:28 -0500

    rutasserver

--- resources/views/cliente/home/pedidos.blade.php
+++ resources/views/cliente/home/pedidos.blade.php
@@ -7,7 +7,7 @@
 
 <h1>EL CARRITO DE COMPRA ESTA VACIO </h1>
 <P>
-    as click <a href="http://127.0.0.1:8000/home">aqui</a> para seguir en el sito
+    as click <a href="http://3.128.26.50:81">aqui</a> para seguir en el sito
 </P>
 
 @endsection
