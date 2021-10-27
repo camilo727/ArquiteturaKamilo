8f4aa0c81a415ced5acad17b4ce17deffcf574e3
camilo727 <53825460+camilo727@users.noreply.github.com>
2021-10-27 09:12:35 -0500

    compratallaagenero

--- resources/views/cliente/home/compra.blade.php
+++ resources/views/cliente/home/compra.blade.php
@@ -33,7 +33,7 @@
     </div>
     <fieldset class="form-group">
         <div class="row">
-            <legend class="col-form-label col-sm-2 pt-0">TALLA</legend>
+            <legend class="col-form-label col-sm-2 pt-0"> GENERO</legend>
             <div class="col-sm-10">
                 <div class="form-check">
                     <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
